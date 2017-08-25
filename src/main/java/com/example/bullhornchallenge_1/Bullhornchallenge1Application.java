819465a5a2d68aa9cc4736b0a56687a353676a6d
RVSW317007DT <RVSW317007DT@MC.MONTGOMERYCOLLEGE.EDU>
2017-08-25 10:27:55 -0400

    Initial commit

---
+++ src/main/java/com/example/bullhornchallenge_1/Bullhornchallenge1Application.java
@@ -0,0 +1,12 @@
+package com.example.bullhornchallenge_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bullhornchallenge1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bullhornchallenge1Application.class, args);
+	}
+}
